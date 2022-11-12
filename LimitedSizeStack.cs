d9d41702fb694c85ae38795637144f6c0af6fafc
Eatmeta <metagroul@gmail.com>
2022-11-12 22:34:50 +0300

    Implement stack of limited size. The Push operation must have O(1) complexity

--- LimitedSizeStack.cs
+++ LimitedSizeStack.cs
@@ -1,29 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TodoApplication
 {
     public class LimitedSizeStack<T>
     {
-        public LimitedSizeStack(int limit)
+        private int _limit;
+        private readonly LinkedList<T> _linkedListStack = new LinkedList<T>();
+
+        public int Limit
         {
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("History length must be non negative");
+                _limit = value;
+            }
+            get => _limit;
         }
+
+        public LimitedSizeStack(int limit) => _limit = limit;
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            if (_limit == 0)
+                return;
+            if (_linkedListStack.Count < _limit)
+                _linkedListStack.AddLast(item);
+            else
+            {
+                _linkedListStack.RemoveFirst();
+                _linkedListStack.AddLast(item);
+            }
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (_linkedListStack.Count == 0)
+                return (T) new object();
+
+            var last = _linkedListStack.Last;
+            _linkedListStack.RemoveLast();
+            return last.Value;
         }
 
-        public int Count
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public int Count => _linkedListStack.Count;
     }
 }
